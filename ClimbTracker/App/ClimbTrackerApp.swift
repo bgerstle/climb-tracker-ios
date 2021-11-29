c481f5dd47d229dbbc982dfca8b90a42c7626939
Brian Gerstle <brian.gerstle@gmail.com>
2021-11-29 16:12:37 -0600

    refactor App

--- ClimbTracker/App/ClimbTrackerApp.swift
+++ ClimbTracker/App/ClimbTrackerApp.swift
@@ -10,88 +10,16 @@ import Combine
 import GRDB
 import os
 
-extension ProcessInfo {
-    var isTesting: Bool {
-        environment["XCTestConfigurationFilePath"] != nil
-    }
-}
-extension Logger {
-    static func app(category: String) -> Logger {
-        Logger(subsystem: Bundle.main.bundleIdentifier ?? "unknownBundleID", category: category)
-    }
-}
-
 @main
 struct ClimbTrackerApp: App {
-    enum DBSetupError : Error {
-        case pathNotFound(path: String)
-    }
-
     let logger = Logger.app(category: "main")
 
-    func databasePath() throws -> String {
-        try FileManager.default
-            .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-            .appendingPathComponent("db.sqlite")
-            .path
-    }
-
-    func setupDatabase() throws -> DatabasePool {
-        // TODO: show error page if this fails
-        let dbPath = try databasePath(),
-            db = try DatabasePool(path: dbPath)
-
-        var migrator = DatabaseMigrator()
-        migrator.setupEventStoreMigrations()
-
-        try migrator.migrate(db)
-
-        return db
-    }
-
-    func resetDatabase() throws {
-        let dbPath = try databasePath()
-
-        try FileManager.default.removeItem(atPath: dbPath)
-    }
-
-    func importCSV(projectService: ProjectService, projectNameService: ProjectNameService) throws {
-        let filename = "export_2019_to_2021_05_23"
-        var imports = UserDefaults.standard.object(forKey: "imports") as? Array<AnyObject> ?? [AnyObject]()
-
-        if imports.contains(where: { ($0 as? String) == filename }) {
-            logger.info("Already imported \(filename)")
-            return
-        }
-
-        let inputURL = Bundle.main.url(forResource: filename, withExtension: "csv")!
-
-        let importer = CSVImporter<CSVRow>(projectService: projectService,
-                                           projectNameService: projectNameService)
-
-        let semaphore = DispatchSemaphore(value: 0)
-
-        Task.detached {
-            logger.info("Importing \(filename)")
-            do {
-                try await importer.importCSV(inputURL)
-            } catch {
-                logger.fault("Failed to import CSV \(error.localizedDescription)")
-            }
-            semaphore.signal()
-        }
-
-        let _ = semaphore.wait(timeout: DispatchTime.distantFuture)
-
-
-        imports.append(filename as NSString)
-        UserDefaults.standard.set(imports, forKey: "imports")
-        logger.info("Done importing \(filename)")
-    }
+    let dbManager = DatabaseManager()
+    let csvImportManager = CSVImportManager()
 
     var body: some Scene {
-        // FIXME remove try!
-        let db = try! setupDatabase(),
+        // FIXME: remove try!
+        let db = try! dbManager.setupDatabase(),
             eventStore = try! PersistentEventStore(db: db),
             projectService = ProjectEventService(eventStore: eventStore),
             projectNameService = ProjectNameEventService(eventStore: eventStore),
@@ -100,10 +28,20 @@ struct ClimbTrackerApp: App {
 
         if !ProcessInfo.processInfo.isTesting {
             if CommandLine.arguments.contains("-resetDatabase") {
-                try! resetDatabase()
+                try! dbManager.resetDatabase()
             }
 
-            try! importCSV(projectService: projectService, projectNameService: projectNameService)
+            // TODO: proper async importing & UI
+            // if I try to extract this, it deadlocks?!
+            let semaphore = DispatchSemaphore(value: 0)
+
+            Task.detached {
+                try! await self.csvImportManager.importCSV(projectService: projectService, projectNameService: projectNameService)
+                semaphore.signal()
+            }
+
+            semaphore.wait()
+
             let summaryEventPublisher = summarizer.summarizeProjectEvents(
                 boulder: projectService.boulderProjectEventPublisher,
                 rope: projectService.ropeProjectEventPublisher,
@@ -123,5 +61,16 @@ struct ClimbTrackerApp: App {
                 viewModel: projectListViewModel
             )
         }
+    }
+}
+
+extension ProcessInfo {
+    var isTesting: Bool {
+        environment["XCTestConfigurationFilePath"] != nil
+    }
+}
+extension Logger {
+    static func app(category: String) -> Logger {
+        Logger(subsystem: Bundle.main.bundleIdentifier ?? "unknownBundleID", category: category)
     }
 }
---
+++ ClimbTracker/App/DatabaseManager.swift
@@ -0,0 +1,40 @@
+//
+//  DatabaseManager.swift
+//  ClimbTracker
+//
+//  Created by Brian Gerstle on 11/29/21.
+//
+
+import Foundation
+import GRDB
+
+class DatabaseManager {
+    enum SetupError : Error {
+        case pathNotFound(path: String)
+    }
+
+    private var migrator = DatabaseMigrator()
+
+    private func databasePath() throws -> String {
+        try FileManager.default
+            .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent("db.sqlite")
+            .path
+    }
+
+    func setupDatabase() throws -> DatabasePool {
+        // TODO: show error page if this fails
+        let dbPath = try databasePath(),
+            db = try DatabasePool(path: dbPath)
+
+        migrator.setupEventStoreMigrations()
+
+        try migrator.migrate(db)
+
+        return db
+    }
+
+    func resetDatabase() throws {
+        try FileManager.default.removeItem(atPath: try databasePath())
+    }
+}
